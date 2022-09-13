2d29068e29a7cfc028f6e35ac7326641b577e3f7
Steven Lai <wai30115@live.hk>
2022-09-12 20:53:59 -0400

    code formatting

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
 const express = require("express");
 const res = require("express/lib/response");
 
-const app =express();
+const app = express();
 
 console.log("Server is listening at http://127.0.0.1:9000/");
-console.log("Endpoints: http://127.0.0.1:9000/images \nmethod: GET, POST, DELETE" );
+console.log(
+  "Endpoints: http://127.0.0.1:9000/images \nmethod: GET, POST, DELETE"
+);
 
-// Endpoint 
+// Endpoint
 
 // GET
-app.get('/images',(req,res)=>{
-    console.log("products GET: received request");
-    res.send ("getting");
+app.get("/images", (req, res) => {
+  console.log("products GET: received request");
+  res.send("getting");
 });
 
 // POST
-app.post('/images',(req,res)=>{
-    console.log("products POST: received request");
-    res.send ("Posting");
+app.post("/images", (req, res) => {
+  console.log("products POST: received request");
+  res.send("Posting");
 });
 
 // DELETE
-app.delete('/images',(req,res)=>{
-    console.log("products DELETE: received request");
-    res.send ("Deleting");
+app.delete("/images", (req, res) => {
+  console.log("products DELETE: received request");
+  res.send("Deleting");
 });
-
 
 // server listening on port 9000
 app.listen(9000);
